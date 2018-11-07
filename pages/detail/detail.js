bb7df11ccdd2524de7c099135020ab212af47e0a
Li Rui <lirui_working@163.com>
2018-11-07 12:52:10 +0800

    Modify according suggestions to add sources.

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -18,7 +18,7 @@ Page({
       success: res => {
         let newDetail = res.data.result
         let date = newDetail.date;
-        newDetail.date = date.slice(0, 10) + ' ' + date.slice(11, 16)
+        newDetail.date = date.slice(5, 10) + ' ' + date.slice(11, 16)
         this.setData({
           newDetail: res.data.result,
         })
@@ -32,53 +32,4 @@ Page({
   onLoad: function (options) {
     this.getDetail(options.id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
